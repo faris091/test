956a832103ab03e3b69ab2e8eba1ed98c41bda7f
faris091 <gost_faris@yahoo.com>
2016-03-16 13:13:28 +0800

    v1.1

--- Kippo/kippo.py
+++ Kippo/kippo.py
@@ -1,3 +1,26 @@
+if [ -d "$script_dir" ];
+then
+	cp /honeypot-setup-script/templates/kippo.cfg.tmpl /tmp/kippo.cfg
+else
+	
+	sudo wget https://raw.github.com/andrewmichaelsmith/honeypot-setup-script/master/templates/kippo.cfg.tmpl -O /tmp/kippo.cfg
+fi
+
+if [ $(dpkg-query -W -f='${Status}' sudo 2>/dev/null | grep -c "ok installed") -eq 0 ]
+then
+  #sudo package is not currently installed on this box
+  echo '[Error] Please install sudo before contniuing (apt-get install sudo)'
+  exit 1
+fi
+
+current_user=$(whoami)
+
+if [ $(sudo -n -l -U ${current_user} 2>&1 | egrep -c -i "not allowed to run sudo|unknown user") -eq 1 ]
+then
+   echo '[Error]: You need to run this script under an account that has access to sudo'
+   exit 1
+fi
+
 sudo apt-get update &> /dev/null
 
 sudo sed -i 's:Port 22:Port 65534:g' /etc/ssh/sshd_config

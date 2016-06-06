8b63d4f4d1fa5795a450a820fd0e1e2cc20677a5
faris091 <gost_faris@yahoo.com>
2016-06-07 01:48:40 +0800

    skrip test

---
+++ run smtp.py
@@ -0,0 +1,10 @@
+sudo apt-get ssmtp
+cd /etc/ssmtp
+sudo rm -f ssmtp.conf
+sudo wget https://raw.githubusercontent.com/faris091/Test/Kippo/ssmtp.conf
+sudo sed -i 's:root=postmaster:root=MyEmails:g' /etc/ssmtp/ssmtp.conf
+sudo sed -i 's:mailhub=mail:mailhub=smtp.gmail.com:587:g' /etc/ssmtp/ssmtp.conf
+sudo sed -i 's:AuthUser=user:AuthUser=MyEmail:g' /etc/ssmtp/ssmtp.conf
+sudo sed -i 's:AuthPass=pass:AuthPass=MyPassword:g' /etc/ssmtp/ssmtp.conf
+sudo sed -i 's:rewriteDomain=:rewriteDomain=gmail.com:g' /etc/ssmtp/ssmtp.conf
+sudo sed -i 's:hostname=Ubuntu:hostname=MyEmail:g' /etc/ssmtp/ssmtp.conf
